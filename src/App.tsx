137f0f3b17ad0a1a52d66ee62653f3236376cc54
Alisson Rego <alissondroid07@gmail.com>
2024-04-14 22:51:23 -0300

    resolvida a instalação do sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import Header from './components/Header/Header'
 import './styles/global.scss'
 
 function App() {
 
   return (
     <>
-      <h1>Ola mundo</h1>
+      <Header />
     </>
   )
 }
