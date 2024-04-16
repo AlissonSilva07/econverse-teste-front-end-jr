9db523aac2b973bb0e6ad5f8a95a55287a9dfaaa
Alisson Rego <alissondroid07@gmail.com>
2024-04-16 15:44:35 -0300

    adicionado json para buscar produtos

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -2,29 +2,31 @@ import '../sass/components/_carousel.scss'
 import ProductCard from './ProductCard'
 
 import { useEffect, useState } from 'react'
-import { Product } from '../models/Product'
-import { baseUrl } from '../paths'
+import { Product } from '../types/Product'
 
 const Carousel = () => {
 
     const [ products, setProducts ] = useState<Product[]>([])
 
     useEffect(() => {
-        fetch(baseUrl, {
-            method: "GET",
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'
-            }
+        fetch('produtos.json', {
+            headers : { 
+              'Content-Type': 'application/json',
+              'Accept': 'application/json'
+             }
         })
         .then(resp => resp.json())
-        .then(data => setProducts(data))
+        .then(data => {
+            setProducts(data.products)
+        })
         .catch(err => console.error(err))
     }, [])
 
   return (
     <div className='carousel'>
-        {products && products.map(p => <ProductCard product={p} />)}
+        {products && products.length > 0 && products.map(p => (
+            <ProductCard key={p.productName} product={p} />
+        ))}
     </div>
   )
 }
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { Product } from '../models/Product'
+import { Product } from '../types/Product'
 import '../sass/components/_product_card.scss'
 import ButttonV1 from './ButttonV1'
 
