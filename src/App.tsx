f3fbf793f21bd6d5bb87e8f31b5e83d44449fccc
Alisson Rego <alissondroid07@gmail.com>
2024-04-22 14:14:00 -0300

    adição do componente de marcas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Categories from './components/Categories'
 import Header from './components/Header'
 import Hero from './components/Hero'
+import Marcas from './components/Marcas'
 import Parceiros from './components/Parceiros'
 import Produtos from './components/Produtos'
 import Related from './components/Related'
@@ -18,6 +19,7 @@ function App() {
       <Parceiros />
       <SectionTitle title='Produtos Relacionados' sub='Ver Todos' />
       <Produtos />
+      <Marcas />
     </>
   )
 }
